58e0490eb1e772f15852cec221a7c357190f4185
barry-sudo <barry@retail-magnet.com>
2025-09-25 09:40:29 -0400

    Update and rename common_index_reexport.ts to index.ts

--- shop-window-frontend/src/components/common/index.ts
+++ shop-window-frontend/src/components/common/index.ts
@@ -2,3 +2,4 @@ export { ErrorBoundary } from './ErrorBoundary';
 export { LoadingSpinner } from './LoadingSpinner';
 export { ErrorMessage } from './ErrorMessage';
 export { SearchBar } from './SearchBar';
+export { MapErrorBoundary } from './MapErrorBoundary';
