ef95cfcda7fc08c2b19f7c6cb14d8db765864b51
barry-sudo <barry@retail-magnet.com>
2025-09-25 09:39:25 -0400

    Add files via upload

---
+++ shop-window-frontend/src/components/common/common_index_reexport.ts
@@ -0,0 +1,4 @@
+export { ErrorBoundary } from './ErrorBoundary';
+export { LoadingSpinner } from './LoadingSpinner';
+export { ErrorMessage } from './ErrorMessage';
+export { SearchBar } from './SearchBar';
